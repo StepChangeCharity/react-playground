b02b4ee50fbb3a1123cbd407344b44163c116acf
Franz Junge <bitbucket@junge.f9.co.uk>
2015-09-14 14:15:15 +0100

    Updating dependencies

--- install.cmd
+++ install.cmd
@@ -1,6 +1,17 @@
+REM https://robots.thoughtbot.com/setting-up-webpack-for-react-and-hot-module-replacement
 
-call npm install webpack -g
+call npm install webpack --global
+call npm install webpack --save-dev
 call npm update
 call npm install babel-loader --save-dev
+
+
+npm install webpack-dev-server --global
+npm install webpack-dev-server --save-dev
+npm install react-hot-loader --save-dev
+
+
+REM Run up http://localhost:8080
+webpack-dev-server 
 
 webpack -w
