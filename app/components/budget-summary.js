0a7a2fcdfea5e97cd3dd471ea46c6ed98ccf3d70
Franz Junge <bitbucket@junge.f9.co.uk>
2015-09-17 12:56:08 +0100

    Added budget summary to illustrate component comms with flux

---
+++ app/components/budget-summary.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var React = require("react");
+
+
+var budgetSummary = React.createClass({
+  getInitialState: function() {
+    totalIncome: 0,
+    totalExpenditure: 0,
+    surplus: 0
+  },
+
+  render: function() {
+    console.log("budgetSummary::render");
+    
+    return (
+      <div>
+        <h3>Summary</h3>
+
+        <table class="mui-table">
+          <thead>
+            <tr><th>Field</th><th>Amount</th></tr>
+          </thead>
+          <tbody>
+            <tr><td>Income</td><td>{this.state.totalIncome}</td></tr>
+            <tr><td>Expenditure</td><td>{this.state.totalExpenditure}</td></tr>
+            <tr><td>Surplus</td><td>{this.state.surplus}</td></tr>
+          </tbody>
+        </table>
+
+      </div>
+    )
+  },
+
+});
+
+module.exports = budgetSummary;
